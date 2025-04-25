fbd2135d41ac11e95b4afa3bfc6047bab67b93e7
Vinicius <viniciusbuenodeoliveira2017@gmail.com>
2025-04-25 11:20:50 -0300

    Funções de upload e conexão com o banco

---
+++ client/lib/features/home/viewmodel/home_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeViewModelHash() => r'4ef2b751cfbd09415ca47cfb591b523556ace0a0';
+
+/// See also [HomeViewModel].
+@ProviderFor(HomeViewModel)
+final homeViewModelProvider =
+    AutoDisposeNotifierProvider<HomeViewModel, AsyncValue?>.internal(
+  HomeViewModel.new,
+  name: r'homeViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homeViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HomeViewModel = AutoDisposeNotifier<AsyncValue?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- server/routes/song.py
+++ server/routes/song.py
@@ -9,6 +9,8 @@ from middleware.auth_middleware import auth_middleware
 from dotenv import load_dotenv
 import os
 
+from models.song import Song
+
 load_dotenv()
 
 api_secret = os.getenv("IMAGE_API_SECRET")
@@ -22,7 +24,7 @@ cloudinary.config(
     secure=True
 )
 
-@router.post('/upload')
+@router.post('/upload', status_code=201)
 def upload_song(song: UploadFile = File(...), 
                 thumbnail: UploadFile = File(...), 
                 artist: str = Form(...), 
@@ -32,9 +34,21 @@ def upload_song(song: UploadFile = File(...),
                 auth_dict = Depends(auth_middleware)):
   
   song_id = str(uuid.uuid4())
-  song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
-  print(song_res)
-  thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
-  print(thumbnail_res)
 
-  return 'ok'
+  song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
+  
+  thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
+
+  new_song = Song(
+    id=song_id,
+    song_name=song_name,
+    artist=artist,
+    hex_code=hex_code,
+    song_url=song_res['url'],
+    thumbnail_url=thumbnail_res['url']
+  )
+
+  db.add(new_song)
+  db.commit()
+  db.refresh(new_song)
+  return new_song
